ec78d3b4bd66331313aa3d43e23495fac9e7baa1
Yuvraaj Narula <ynarula678@gmail.com>
2025-09-24 14:33:24 +0530

    logger config

--- backend/server.js
+++ backend/server.js
@@ -2,17 +2,19 @@ require('dotenv').config();
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
+const logger = require('./config/logger');
 
+logger.info('Logger initialized');
 
 var app = express();
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(process.env.DB_URI, { useNewUrlParser: true, useUnifiedTopology: true }).then(()=>{
-    console.log("Connected to DB");
+    logger.info("Connected to DB");
     app.listen(PORT , ()=>{
-        console.log(`Server started on port ${PORT}`);
+        logger.info(`Server started on port ${PORT}`);
     })
 })
 .catch((err)=>{ 
-    console.log("Error connecting to DB: ", err);
+    logger.error("Error connecting to DB: ", err);
 })
